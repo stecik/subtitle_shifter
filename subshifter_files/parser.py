5253e345b4b1a574ab9d30c687caf3749deb5b15
Marek Štec <mar.stec@seznam.cz>
2022-03-12 12:01:39 +0100

    Parser created, -s,-m,-h, input_file, -f arguments added.

--- subshifter_files/parser.py
+++ subshifter_files/parser.py
@@ -1,19 +1,23 @@
 import argparse
 from constants import DESCRIPTION, PROGRAM_SHORTCUT
+import os
 
 # Functions
-def length_value_limit(arg):
+def integer(arg):
     # limits arguments to positive integers
     try:
         argument = int(arg)
     except ValueError:
         raise argparse.ArgumentTypeError("Must be an integer")
-    if argument < 0:
-        raise argparse.ArgumentTypeError("Argument must be positive")
     return argument
 
 parser = argparse.ArgumentParser(prog=PROGRAM_SHORTCUT, description=DESCRIPTION)
 
+parser.add_argument("input_file", type=str, required=True, help="Specify intput file name")
 
+parser.add_argument("-s", "--sec", type=integer, default=0, help="Time for resync in seconds")
+parser.add_argument("-m", "--min", type=integer, default=0, help="Time for resync in seconds")
+parser.add_argument("-h", "--hrs", type=integer, default=0, help="Time for resync in seconds")
+parser.add_argument("-f", "--format", type=str, default="srt", help="Specify file format (extension)")
 
 args = parser.parse_args()
