035ecaac27ce66378ac7c7ef219fbbc8204a1fee
Marek Štec <mar.stec@seznam.cz>
2022-04-13 13:28:02 +0200

    Exceptions specified

--- subshifter_files/subshifter.py
+++ subshifter_files/subshifter.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     subshifter = SubShifter()
     try:
         subshifter.shift(args.input_file, args.hrs, args.min, args.sec, args.mil)
-    except:
-        print("Error")
+    except Exception as e:
+        print(f"Error: {e}")
 
